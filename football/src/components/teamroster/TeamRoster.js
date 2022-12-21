85166d1ff74f0003c1a396d8e68ab0d40392253a
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-12-21 11:17:34 -0500

    Adding more filters

--- football/src/components/teamroster/TeamRoster.js
+++ football/src/components/teamroster/TeamRoster.js
@@ -3,6 +3,7 @@ import { useState, useEffect } from 'react'
 import { useParams } from "react-router-dom"
 import Card from "../card/Card"
 import './TeamRoster.css'
+import Loading from "../loading/Loading"
 
 
 const TeamRoster = () => {
@@ -34,17 +35,40 @@ const TeamRoster = () => {
   }, [])
 
 
+  if (!roster) return <Loading />
+
+
   let wholeRoster = roster.filter((status) => status.Status === 'Active')
   let displayData = wholeRoster
 
   console.log(wholeRoster)
 
-
+  let Offense = wholeRoster?.filter((position) => position.PositionCategory === 'OFF')
   let QuarterBacks = wholeRoster?.filter((position) => position.Position === 'QB')
   let RunningBacks = wholeRoster?.filter((position) => position.Position === 'RB')
-  let DefensiveBacks = wholeRoster?.filter((position) => position.Position === 'DB')
   let FullBack = wholeRoster?.filter((position) => position.Position === 'FB')
   let Wideouts = wholeRoster?.filter((position) => position.Position === 'WR')
+  let TightEnd = wholeRoster?.filter((position) => position.Position === 'TE')
+  let OffensiveLine = wholeRoster?.filter((position) => position.Position === 'OL')
+  let OffensiveGuard = wholeRoster?.filter((position) => position.Position === 'G')
+  let Center = wholeRoster?.filter((position) => position.Position === 'C')
+  let OffensiveTackle = wholeRoster?.filter((position) => position.Position === 'OT')
+  let Defense = wholeRoster?.filter((position) => position.PositionCategory === 'DEF')
+  let DefensiveTackle = wholeRoster?.filter((position) => position.Position === 'DT')
+  let DefensiveEnd = wholeRoster?.filter((position) => position.Position === 'DE')
+  let LineBackers = wholeRoster?.filter((position) => position.Position === 'LB')
+  let InsideLineBackers = wholeRoster?.filter((position) => position.Position === 'ILB')
+  let OutsideLineBackers = wholeRoster?.filter((position) => position.Position === 'OLB')
+  let DefensiveBacks = wholeRoster?.filter((position) => position.Position === 'DB')
+  let CornerBacks = wholeRoster?.filter((position) => position.Position === 'CB')
+  let Safety = wholeRoster?.filter((position) => position.Position === 'S')
+  let SpecialTeams = wholeRoster?.filter((position) => position.PositionCategory === 'ST')
+  let Kicker = wholeRoster?.filter((position) => position.Position === 'K')
+  let Punter = wholeRoster?.filter((position) => position.Position === 'P')
+  let LongSnapper = wholeRoster?.filter((position) => position.Position === 'LS')
+
+
+  console.log(DefensiveBacks.length > 0 ? "Defensive Backs" : "NA")
 
 
   const handleChange = (event) => {
@@ -54,12 +78,11 @@ const TeamRoster = () => {
   if (initialRoster === "ALL") {
     displayData = wholeRoster
   }
-
+  if (initialRoster === 'Offense') {
+    displayData = Offense
+  }
   if (initialRoster === 'QuarterBacks') {
     displayData = QuarterBacks
-  }
-  if (initialRoster === 'DefensiveBacks') {
-    displayData = DefensiveBacks
   }
   if (initialRoster === "RunningBacks") {
     displayData = RunningBacks
@@ -70,6 +93,70 @@ const TeamRoster = () => {
   if (initialRoster === 'Wideout') {
     displayData = Wideouts
   }
+  if (initialRoster === 'TightEnd') {
+    displayData = TightEnd
+  }
+  if (initialRoster === 'Offensive Guard') {
+    displayData = OffensiveGuard
+  }
+  if (initialRoster === 'Offensive Tackle') {
+    displayData = OffensiveTackle
+  }
+  if (initialRoster === 'Offensive Line') {
+    displayData = OffensiveLine
+  }
+  if (initialRoster === 'Center') {
+    displayData = Center
+  }
+  if (initialRoster === 'Defense') {
+    displayData = Defense
+  }
+  if (initialRoster === 'DE') {
+    displayData = DefensiveEnd
+  }
+  if (initialRoster === 'DT') {
+    displayData = DefensiveTackle
+  }
+  if (initialRoster === 'LB') {
+    displayData = LineBackers
+  }
+  if (initialRoster === 'ILB') {
+    displayData = InsideLineBackers
+  }
+  if (initialRoster === 'OLB') {
+    displayData = OutsideLineBackers
+  }
+  if (initialRoster === 'DefensiveBacks') {
+    displayData = DefensiveBacks
+  }
+  if (initialRoster === 'CornerBacks') {
+    displayData = CornerBacks
+  }
+  if (initialRoster === 'Safety') {
+    displayData = Safety
+  }
+  if (initialRoster === 'SpecialTeams') {
+    displayData = SpecialTeams
+  }
+  if (initialRoster === 'Kicker') {
+    displayData = Kicker
+  }
+  if (initialRoster === 'Punter') {
+    displayData = Punter
+  }
+  if (initialRoster === 'LongSnapper') {
+    displayData = LongSnapper
+  }
+
+  let doHaveOG = OffensiveGuard.length > 0 ? "Offensive Guard" : "N/A"
+  let doHaveOT = OffensiveTackle.length > 0 ? "Offensive Tackle" : "N/A"
+  let doHaveOL = OffensiveLine.length > 0 ? "Offensive Line" : "N/A"
+  let doHaveCenter = Center.length > 0 ? "Center" : "N/A"
+  let doHaveDB = DefensiveBacks.length > 0 ? "Defensive Backs" : "N/A"
+  let doHaveCB = CornerBacks.length > 0 ? "Corner Backs" : "N/A"
+  let doHaveFB = FullBack.length > 0 ? "Full Backs" : "N/A"
+  let doHaveOLB = OutsideLineBackers.length > 0 ? "OutsideLineBackers" : "N/A"
+  let doHaveILB = InsideLineBackers.length > 0 ? "InsideLineBackers" : "N/A"
 
 
   return (
@@ -77,18 +164,41 @@ const TeamRoster = () => {
       <label>
         <select value={filteredResults} onChange={handleChange}>
           <option multiple={false} value={"ALL"}>ALL</option>
+          <option multiple={false} value={"Offense"}>Offense</option>
+          <option>- - --</option>
           <option multiple={false} value={"QuarterBacks"}>Quarterbacks</option>
           <option multiple={false} value={"RunningBacks"}>Runningbacks</option>
-          <option multiple={false} value={"DefensiveBacks"}>Defensivebacks</option>
-          <option multiple={false} value={"FullBack"}>FullBack</option>
+          <option multiple={false} value={"FullBack"}>{doHaveFB}</option>
           <option multiple={false} value={"Wideout"}>Wideouts</option>
+          <option multiple={false} value={"TightEnd"}>Tight End</option>
+          <option multiple={false} value={"Offensive Line"}>{doHaveOL}</option>
+          <option multiple={false} value={"Center"}>{doHaveCenter}</option>
+          <option multiple={false} value={"Offensive Guard"}>{doHaveOG}</option>
+          <option multiple={false} value={"Offensive Tackle"}>{doHaveOT}</option>
+          <option multiple={false} value={"Defense"}>Defense</option>
+          <option>- - --</option>
+          <option multiple={false} value={"DT"}>Defensive Tackle</option>
+          <option multiple={false} value={"DE"}>Defensive End</option>
+          <option multiple={false} value={"LB"}>LineBacker</option>
+          <option multiple={false} value={"ILB"}>{doHaveILB}</option>
+          <option multiple={false} value={"OLB"}>{doHaveOLB}</option>
+          <option multiple={false} value={"DefensiveBacks"}>{doHaveDB}</option>
+          <option multiple={false} value={"CornerBacks"}>{doHaveCB}</option>
+          <option multiple={false} value={"Safety"}>Safety</option>
+          <option multiple={false} value={"SpecialTeams"}>Special Teams</option>
+          <option>- - --</option>
+          <option multiple={false} value={"Kicker"}>Kicker</option>
+          <option multiple={false} value={"Punter"}>Punter</option>
+          <option multiple={false} value={"LongSnapper"}>Long Snapper</option>
+
+
         </select>
       </label>
       {displayData?.map(({ FirstName, PlayerID, LastName, Number, Age, BirthDate, College, Height, Position, PhotoUrl, Weight, ExperienceString }) => (
         <Card key={PlayerID}>
           <h2>{FirstName} {LastName}</h2>
           <img src={PhotoUrl} />
-          <h4>{Number}</h4>
+          <h4>#{Number}</h4>
           <h4>{Height}</h4>
           <h4>{Weight}</h4>
           <h4>{Position}</h4>
